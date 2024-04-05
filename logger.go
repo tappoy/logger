d5f24e6cf193d7397c9b9afa773d52a2e8bbde59
tappoy <tappoy1234@gmail.com>
2024-04-05 20:45:25 +0900

    go fmt

--- logger.go
+++ logger.go
@@ -1,107 +1,105 @@
 package logger
 
 import (
-  "os"
-  "errors"
-  "path/filepath"
-  "fmt"
-  "time"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 type Logger struct {
-  logDirPath string
-  logFileName string
-  logLevel string
+	logDirPath  string
+	logFileName string
+	logLevel    string
 }
 
 var logLevelMap = map[string]int{
-  "DEBUG": 0,
-  "INFO": 1,
-  "ERROR": 2,
-  "NONE": 3,
+	"DEBUG": 0,
+	"INFO":  1,
+	"ERROR": 2,
+	"NONE":  3,
 }
 
 func NewLogger(logDirPath string, logFileName string) (*Logger, error) {
-  // create log directory if not exists
-  if _, err := os.Stat(logDirPath); os.IsNotExist(err) {
-    err := os.MkdirAll(logDirPath, 0755)
-    if err != nil {
-      return nil, errors.New("ErrCannotCreateLogDir")
-    }
-  }
+	// create log directory if not exists
+	if _, err := os.Stat(logDirPath); os.IsNotExist(err) {
+		err := os.MkdirAll(logDirPath, 0755)
+		if err != nil {
+			return nil, errors.New("ErrCannotCreateLogDir")
+		}
+	}
 
-  // create log file if not exists
-  logFilePath := filepath.Join(logDirPath, logFileName)
-  if _, err := os.Stat(logFilePath); os.IsNotExist(err) {
-    _, err := os.Create(filepath.Join(logDirPath, logFileName))
-    if err != nil {
-      return nil, errors.New("ErrCannotCreateLogFile")
-    }
-  }
+	// create log file if not exists
+	logFilePath := filepath.Join(logDirPath, logFileName)
+	if _, err := os.Stat(logFilePath); os.IsNotExist(err) {
+		_, err := os.Create(filepath.Join(logDirPath, logFileName))
+		if err != nil {
+			return nil, errors.New("ErrCannotCreateLogFile")
+		}
+	}
 
-  // check log file write permission
-  if _, err := os.OpenFile(logFilePath, os.O_WRONLY, 0666); err != nil {
-    return nil, errors.New("ErrCannotWriteLogFile")
-  }
+	// check log file write permission
+	if _, err := os.OpenFile(logFilePath, os.O_WRONLY, 0666); err != nil {
+		return nil, errors.New("ErrCannotWriteLogFile")
+	}
 
-  // create logger
-  logger := &Logger{
-    logDirPath: logDirPath,
-    logFileName: logFileName,
-    logLevel: "INFO",
-  }
+	// create logger
+	logger := &Logger{
+		logDirPath:  logDirPath,
+		logFileName: logFileName,
+		logLevel:    "INFO",
+	}
 
-  return logger, nil
+	return logger, nil
 }
 
 func (logger *Logger) SetLogLevel(logLevel string) error {
-  if _, ok := logLevelMap[logLevel]; ok {
-    logger.logLevel = logLevel
-    return nil
-  }
+	if _, ok := logLevelMap[logLevel]; ok {
+		logger.logLevel = logLevel
+		return nil
+	}
 
-  return errors.New("ErrInvalidLogLevel")
+	return errors.New("ErrInvalidLogLevel")
 }
 
 func (logger *Logger) log(level string, format string, args ...interface{}) {
-  // string to int log level
-  levelInt := logLevelMap[level]
-  limitInt := logLevelMap[logger.logLevel]
+	// string to int log level
+	levelInt := logLevelMap[level]
+	limitInt := logLevelMap[logger.logLevel]
 
-  // filter log level
-  if levelInt < limitInt {
-    return
-  }
+	// filter log level
+	if levelInt < limitInt {
+		return
+	}
 
-  // log message
-  logFilePath := filepath.Join(logger.logDirPath, logger.logFileName)
-  logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_WRONLY, 0666)
-  if err != nil {
-    return
-  }
-  defer logFile.Close()
+	// log message
+	logFilePath := filepath.Join(logger.logDirPath, logger.logFileName)
+	logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		return
+	}
+	defer logFile.Close()
 
-  // log header, timestamp, logLevel
-  header := fmt.Sprintf("datetime:%s\tlevel:%s\tlog:", time.Now().Format("2006-01-02 15:04:05"), level)
+	// log header, timestamp, logLevel
+	header := fmt.Sprintf("datetime:%s\tlevel:%s\tlog:", time.Now().Format("2006-01-02 15:04:05"), level)
 
-  // Sprintf log message
-  message := fmt.Sprintf(format, args...)
+	// Sprintf log message
+	message := fmt.Sprintf(format, args...)
 
-  // write log
-  logFile.WriteString(header + message + "\n")
+	// write log
+	logFile.WriteString(header + message + "\n")
 
 }
 
 func (logger *Logger) Debug(format string, args ...interface{}) {
-  logger.log("DEBUG", format, args...)
+	logger.log("DEBUG", format, args...)
 }
 
 func (logger *Logger) Info(format string, args ...interface{}) {
-  logger.log("INFO", format, args...)
+	logger.log("INFO", format, args...)
 }
 
 func (logger *Logger) Error(format string, args ...interface{}) {
-  logger.log("ERROR", format, args...)
+	logger.log("ERROR", format, args...)
 }
-
-
--- logger_test.go
+++ logger_test.go
@@ -1,96 +1,96 @@
 package logger
 
 import (
-  "testing"
-  "path/filepath"
-  "strings"
-  "os"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
 )
 
 var logDirPath = "/tmp"
 var logFileName = "test.log"
 
 func TestMain(m *testing.M) {
-  os.RemoveAll(filepath.Join(logDirPath, logFileName))
-  retCode := m.Run()
-  os.Exit(retCode)
+	os.RemoveAll(filepath.Join(logDirPath, logFileName))
+	retCode := m.Run()
+	os.Exit(retCode)
 }
 
 func TestNewLogger(t *testing.T) {
-  logger, err := NewLogger(logDirPath, logFileName)
-  if err != nil {
-    t.Errorf("NewLogger(%s, %s) = %v", logDirPath, logFileName, err)
-  }
-  if logger == nil {
-    t.Errorf("NewLogger(%s, %s) = %v", logDirPath, logFileName, logger)
-  }
-  if logger.logLevel != "INFO" {
-    t.Errorf("NewLogger(%s, %s) = %v", logDirPath, logFileName, logger.logLevel)
-  }
+	logger, err := NewLogger(logDirPath, logFileName)
+	if err != nil {
+		t.Errorf("NewLogger(%s, %s) = %v", logDirPath, logFileName, err)
+	}
+	if logger == nil {
+		t.Errorf("NewLogger(%s, %s) = %v", logDirPath, logFileName, logger)
+	}
+	if logger.logLevel != "INFO" {
+		t.Errorf("NewLogger(%s, %s) = %v", logDirPath, logFileName, logger.logLevel)
+	}
 }
 
 func TestSetLogLevel(t *testing.T) {
-  logger, _ := NewLogger(logDirPath, logFileName)
-  logLevels := []string{"DEBUG", "INFO", "ERROR", "NONE"}
+	logger, _ := NewLogger(logDirPath, logFileName)
+	logLevels := []string{"DEBUG", "INFO", "ERROR", "NONE"}
 
-  for _, logLevel := range logLevels {
-    err := logger.SetLogLevel(logLevel)
-    if err != nil {
-      t.Errorf("SetLogLevel(%s) = %v", logLevel, err)
-    }
-    if logger.logLevel != logLevel {
-      t.Errorf("SetLogLevel(%s) = %v", logLevel, logger.logLevel)
-    }
-  }
+	for _, logLevel := range logLevels {
+		err := logger.SetLogLevel(logLevel)
+		if err != nil {
+			t.Errorf("SetLogLevel(%s) = %v", logLevel, err)
+		}
+		if logger.logLevel != logLevel {
+			t.Errorf("SetLogLevel(%s) = %v", logLevel, logger.logLevel)
+		}
+	}
 }
 
 func TestSetLogLevelWithInvalidLogLevel(t *testing.T) {
-  logger, _ := NewLogger(logDirPath, logFileName)
-  logLevel := "INVALID"
-  err := logger.SetLogLevel(logLevel)
-  if err == nil {
-    t.Errorf("SetLogLevel(%s) = %v", logLevel, err)
-  }
-  if err.Error() != "ErrInvalidLogLevel" {
-    t.Errorf("SetLogLevel(%s) = %v", logLevel, err)
-  }
+	logger, _ := NewLogger(logDirPath, logFileName)
+	logLevel := "INVALID"
+	err := logger.SetLogLevel(logLevel)
+	if err == nil {
+		t.Errorf("SetLogLevel(%s) = %v", logLevel, err)
+	}
+	if err.Error() != "ErrInvalidLogLevel" {
+		t.Errorf("SetLogLevel(%s) = %v", logLevel, err)
+	}
 }
 
 func TestDebug(t *testing.T) {
-  logger, _ := NewLogger(logDirPath, logFileName)
-  logger.SetLogLevel("DEBUG")
-  logger.Debug("debug message")
+	logger, _ := NewLogger(logDirPath, logFileName)
+	logger.SetLogLevel("DEBUG")
+	logger.Debug("debug message")
 
-  messages, _ := os.ReadFile(filepath.Join(logDirPath, logFileName))
-  messageStr := string(messages)
+	messages, _ := os.ReadFile(filepath.Join(logDirPath, logFileName))
+	messageStr := string(messages)
 
-  if !strings.Contains(messageStr, "level:DEBUG\tlog:debug message") {
-    t.Errorf("Debug() = %v", messageStr)
-  }
+	if !strings.Contains(messageStr, "level:DEBUG\tlog:debug message") {
+		t.Errorf("Debug() = %v", messageStr)
+	}
 }
 
 func TestInfo(t *testing.T) {
-  logger, _ := NewLogger(logDirPath, logFileName)
-  logger.SetLogLevel("INFO")
-  logger.Info("info message")
+	logger, _ := NewLogger(logDirPath, logFileName)
+	logger.SetLogLevel("INFO")
+	logger.Info("info message")
 
-  messages, _ := os.ReadFile(filepath.Join(logDirPath, logFileName))
-  messageStr := string(messages)
+	messages, _ := os.ReadFile(filepath.Join(logDirPath, logFileName))
+	messageStr := string(messages)
 
-  if !strings.Contains(messageStr, "level:INFO\tlog:info message") {
-    t.Errorf("Info() = %v", messageStr)
-  }
+	if !strings.Contains(messageStr, "level:INFO\tlog:info message") {
+		t.Errorf("Info() = %v", messageStr)
+	}
 }
 
 func TestError(t *testing.T) {
-  logger, _ := NewLogger(logDirPath, logFileName)
-  logger.SetLogLevel("ERROR")
-  logger.Error("error message")
+	logger, _ := NewLogger(logDirPath, logFileName)
+	logger.SetLogLevel("ERROR")
+	logger.Error("error message")
 
-  messages, _ := os.ReadFile(filepath.Join(logDirPath, logFileName))
-  messageStr := string(messages)
+	messages, _ := os.ReadFile(filepath.Join(logDirPath, logFileName))
+	messageStr := string(messages)
 
-  if !strings.Contains(messageStr, "level:ERROR\tlog:error message") {
-    t.Errorf("Error() = %v", messageStr)
-  }
+	if !strings.Contains(messageStr, "level:ERROR\tlog:error message") {
+		t.Errorf("Error() = %v", messageStr)
+	}
 }
